f910ce0982ce093fbb70bd4601a29b20edde79ba
RicardoL1u <2018212874@bupt.edu.cn>
2022-10-31 22:20:46 +0800

    fea: new data format relation

--- website/create_table.py
+++ website/create_table.py
@@ -1,4 +1,16 @@
-from sqlalchemy import BOOLEAN, DATETIME, DefaultClause, ForeignKey, create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy import (
+   DATETIME, 
+   Boolean, 
+   ForeignKey, 
+   create_engine, 
+   MetaData, 
+   Table, 
+   Column, 
+   Integer, 
+   String,
+   insert,
+   values
+)
 engine = create_engine('sqlite:///database.db', echo = True)
 meta = MetaData()
 
@@ -6,66 +18,61 @@ manager = Table(
    'manager', meta,
    Column('phone',String, primary_key = True),
    Column('name', String, unique=True, nullable=False),
+   Column('token', String,nullable=False),
 )
 
 annotator = Table(
    'annotator', meta, 
    Column('phone', String,primary_key=True),
    Column('name', String, nullable=False,unique = True), 
-   Column('token', String,nullable=False, unique = True),
+   Column('token', String,nullable=False),
    Column('manager_phone', Integer, ForeignKey('manager.phone')),
-   # Column('id', Integer, primary_key = True, autoincrement = True),
 )
 
 passage = Table(
    'passage', meta, 
    Column('id', Integer, primary_key = True, autoincrement = True),
-   Column('passage_id', String),
-   Column('done',BOOLEAN, DefaultClause(False))
+   Column('passage_id', String, unique=True, nullable=False),
+   Column('done',Boolean, default=False),
 )
 
 annotated_data = Table(
    'annotated_data', meta, 
    Column('id', Integer, primary_key=True, autoincrement=True), 
    Column('passage_id', String, ForeignKey('passage.passage_id')),
-   Column('create_timestamp', DATETIME, DefaultClause("CURRENT_TIMESTAMP")), 
+   Column('create_timestamp', DATETIME, default = "CURRENT_TIMESTAMP"), 
    Column('annotator_phone', String, ForeignKey('annotator.phone')),
-   Column('annotated_filename', String, nullable=False), 
+   Column('annotated_filename', String, nullable=False,unique=True), 
 )
 
 annotator_task = Table(
    'annotator_task', meta,
    Column('id', Integer, primary_key=True, autoincrement=True),
    Column('annotator_phone', String, ForeignKey('annotator.phone')),
-   Column('task_done_number', Integer, DefaultClause("0")),
+   Column('task_done_number', Integer, default=0),
    Column('passage_id', String, ForeignKey('passage.passage_id')),
-   Column('score', Integer, DefaultClause("0"))
+   Column('reviewed', Integer, default=0),
+   Column('annotated_filename', String, ForeignKey('annotated_data.annotated_filename')),
 )
 
 meta.create_all(engine)
 
-# from flask_sqlalchemy import SQLAlchemy
-# from flask import Flask
-# from models import Annotator, Manager, Annotated_Data
-# from werkzeug.security import generate_password_hash, check_password_hash
+import json
+from werkzeug.security import generate_password_hash
+dataset = json.load(open('../data/company_data.json'))
+conn = engine.connect()
 
+conn.execute(passage.insert(),[
+      {'passage_id':data['id']}
+      for data in dataset
+   ]
+)
 
-# db = SQLAlchemy()
-# DB_NAME = "database.db"
+conn.execute(manager.insert(),[
+   {'name':'liu','phone':'110','token':generate_password_hash('password',method='sha256')}
+])
 
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-# db.init_app(app)
-
-
-# new_manager = Manager(manager_name = "test", phone = "123")
-# db.session.add(new_manager)
-# new_annotator = Annotator(
-#    annotator_name = "test_anno", 
-#    token = generate_password_hash('123',method='sha256'), 
-#    phone = "123",
-#    manager_id = 0
-# )
-# db.session.add(new_annotator)
-# db.session.commit()
+conn.execute(annotator.insert(),[
+      {'name':'liu','phone':'156','token':generate_password_hash('password',method='sha256'),'manager_phone':'110'}
+   ]
+)
--- website/models.py
+++ website/models.py
@@ -2,9 +2,41 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
+class Manager(db.Model, UserMixin):
+    phone = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    token = db.Column(db.String, nullable=False)
 
 class Annotator(db.Model,UserMixin):
+    phone = db.Column(db.String, unique=True, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String, unique=True)
+    manager_phone = db.Column(db.String, db.ForeignKey('manager.phone'))
+
+
+class Passage(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    passage_id = db.Column(db.String, unique=True, nullable=False)
+    done = db.Column(db.Boolean,default=False)
+
+class Annotated_Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    annotator_name = db.Column(db.String(30), unique=True)
-    token = db.Column(db.String(30), unique=True)
+    passage_id = db.Column(db.String,db.ForeignKey('passage.id'))
+    create_timestamp = db.Column(db.TIMESTAMP, default=func.now())
+    annotator_phone = db.Column(db.String, db.ForeignKey('annotator.phone'))
+    annotated_filename = db.Column(db.String,nullable=False)
+
+class Annotator_Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    annotator_phone = db.Column(db.String, db.ForeignKey('annotator.phone'))
+    passage_id = db.Column(db.String)
+    task_done_number = db.Column(db.Integer, default=0)
+
+    reviewed = db.Column(db.Integer,default=0) 
+    # set default to 0 as not reviewed yet
+    # when equal to -1 as need re-annotate
+    # when equal to 1 as task has been completed and need to assign one final result
+    annotated_filename = db.Column(db.String, db.ForeignKey('annotated_data.annotated_filename'))
+
+
