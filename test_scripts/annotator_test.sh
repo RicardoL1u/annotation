fbb597da3b846e2d7a2d3e22462652be68688832
RicardoL1u <2018212874@bupt.edu.cn>
2022-11-01 09:35:56 +0800

    fea: annotator login and sign up test

---
+++ test_scripts/annotator_test.sh
@@ -0,0 +1,7 @@
+curl -X POST http://127.0.0.1:5000/annotator/login -c annotator.cookies -H 'Content-Type: application/json' -d '{"id":"156","token":"password"}'
+
+# curl -X POST http://127.0.0.1:5000/annotaotor/get_annotators -b annotator.cookies
+
+# curl -X POST http://127.0.0.1:5000/annotator/assign_task -b annotator.cookies  -H 'Content-Type: application/json' -d '{"annotator_phone":"156","task_start_idx":11,"task_end_idx":13}'
+
+curl -X POST http://127.0.0.1:5000/annotator/logout -b annotator.cookies
