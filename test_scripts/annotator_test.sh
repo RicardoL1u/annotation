7ae2d187433e1c6017d19ab8c727c1a7ba9e7b07
RicardoL1u <2018212874@bupt.edu.cn>
2022-11-01 21:25:15 +0800

    ref: add description to each interface

--- test_scripts/annotator_test.sh
+++ test_scripts/annotator_test.sh
@@ -1,11 +1,16 @@
 curl -X POST http://127.0.0.1:5000/annotator/login -c annotator.cookies -H 'Content-Type: application/json' -d '{"id":"156","token":"password"}'
 
-# curl -X POST http://127.0.0.1:5000/annotaotor/get_annotators -b annotator.cookies
-
-# curl -X POST http://127.0.0.1:5000/annotator/assign_task -b annotator.cookies  -H 'Content-Type: application/json' -d '{"annotator_phone":"156","task_start_idx":11,"task_end_idx":13}'
-
+# get the data of 13th passage
+# the return data format is 
+# dataset[idx] follow the format we argee at 34:/data/lyt/workspace/Seq2Seq-MRC/analysis/company_dataformat.md 
+        # return {
+        #     'message': "data",
+        #     'data':dataset[idx],
+        #     'code': 1
+        # }
 curl -X GET http://127.0.0.1:5000/data/pageID=13 -b annotator.cookies
 
+# "data" contain the annotated result of 13th passage
 curl -X POST http://127.0.0.1:5000/data/pageID=13 -b annotator.cookies -H 'Content-Type: application/json' -d '{"data":{"id":"156","token":"password"}}'
 
 curl -X POST http://127.0.0.1:5000/annotator/logout -b annotator.cookies
--- test_scripts/manager_test.sh
+++ test_scripts/manager_test.sh
@@ -1,7 +1,14 @@
+# manager login with passage and id
+# id is phone
 curl -X POST http://127.0.0.1:5000/manager/login -c manager.cookies -H 'Content-Type: application/json' -d '{"id":"110","token":"password"}'
 
+# get all annotators who belongs to this manager
 curl -X POST http://127.0.0.1:5000/manager/get_annotators -b manager.cookies
 
-curl -X POST http://127.0.0.1:5000/manager/assign_task -b manager.cookies  -H 'Content-Type: application/json' -d '{"annotator_id":"156","task_start_idx":11,"task_end_idx":13}'
+# assign task to one given annotator
+# passage_start_idx is start index of passage
+# passage_end_idx is end index of passage
+curl -X POST http://127.0.0.1:5000/manager/assign_task -b manager.cookies  -H 'Content-Type: application/json' -d '{"annotator_id":"156","passage_start_idx":11,"passage_end_idx":13}'
 
+# manager logout
 curl -X POST http://127.0.0.1:5000/manager/logout -b manager.cookies
