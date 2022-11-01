23f74c9ed95e259c8901a9d48878b100a4f35891
RicardoL1u <2018212874@bupt.edu.cn>
2022-11-01 21:06:05 +0800

    fea: annotator test script

--- test_scripts/annotator_test.sh
+++ test_scripts/annotator_test.sh
@@ -4,4 +4,8 @@ curl -X POST http://127.0.0.1:5000/annotator/login -c annotator.cookies -H 'Cont
 
 # curl -X POST http://127.0.0.1:5000/annotator/assign_task -b annotator.cookies  -H 'Content-Type: application/json' -d '{"annotator_phone":"156","task_start_idx":11,"task_end_idx":13}'
 
+curl -X GET http://127.0.0.1:5000/data/pageID=13 -b annotator.cookies
+
+curl -X POST http://127.0.0.1:5000/data/pageID=13 -b annotator.cookies -H 'Content-Type: application/json' -d '{"data":{"id":"156","token":"password"}}'
+
 curl -X POST http://127.0.0.1:5000/annotator/logout -b annotator.cookies
