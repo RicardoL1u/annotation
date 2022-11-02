0d7fe543107be0f32449addc04cbf0e36e73be81
RicardoL1u <2018212874@bupt.edu.cn>
2022-11-02 20:59:54 +0800

    fea: for cors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
-
+from flask import Flask
+from flask_cors import CORS
 app = create_app()
-
+CORS(app,resources=r'/*',vary_header=False,supports_credentials=True)
 if __name__ == '__main__':
     app.run(debug=True,port=9522,host="0.0.0.0")
