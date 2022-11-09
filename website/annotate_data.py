8f13720b31c0eb14bf23ea8cff500bfb96e7e5c3
RicardoL1u <2018212874@bupt.edu.cn>
2022-11-09 17:25:51 +0800

    ref: the struct of project

--- website/annotate_data.py
+++ website/annotate_data.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from hashlib import sha256
 from .models import Passage, Annotator, AnnotatorTask, AnnotatedData
 annotate_data = Blueprint('annotate_data', __name__)
-dataset = {v['id']:v for v in json.load(open('data/company_data_0.8.json'))}
+dataset = {v['id']:v for v in json.load(open('resource/company_data_0.8.json'))}
 
 
 @annotate_data.route('/pageID=<hash_id>',methods=['GET','POST'])
