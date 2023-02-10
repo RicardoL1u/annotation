9aefafc065d2a6157288ee609d6f150b44ed5292
RicardoL1u <2018212874@bupt.edu.cn>
2023-02-10 23:09:35 +0800

    fea: export annotated datasets

--- instance/export.py
+++ instance/export.py
@@ -5,19 +5,23 @@ conn = sqlite3.connect('database.db')
 print( "Opened database successfully")
 
 
-MANAGER_ID = '15933111848' # AISHU
-# MANAGER_ID = '17392323793' # LONGMAO
+# MANAGER_ID = '15933111848' # AISHU
+# output_file_name = 'aishu.json'
+MANAGER_ID = '17392323793' # LONGMAO
+output_file_name = 'longmao.json'
+
 
 
 annotator_ids = [id[0] for id in conn.execute(f"SELECT id from annotator where manager_id={MANAGER_ID}")]
+print(f'there are {len(annotator_ids)} annotators employed')
 cmd = "SELECT annotated_filename,passage_ori_id,create_timestamp from annotated_data where " + \
     " or ".join([f"annotator_id=\"{id}\"" for id in annotator_ids])
 annotated_datas = [[filename,passage_ori_id,datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')] for filename,passage_ori_id,timestamp in conn.execute(cmd) ]
-
+print(f'there are {len(annotated_datas)} data')
 annotated_datas.sort(key=lambda x:(x[1],x[2]))
 passage_results = {data[1]:data[0] for data in annotated_datas}
-
-with open('aishu.json','w') as f:
+print(f'there are {len(passage_results)} passages')
+with open(output_file_name,'w') as f:
     json.dump(passage_results,f,indent=4)
 
 print( "Operation done successfully")
